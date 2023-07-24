1efda69b86753006240a3e905ecfefba6421a497
CryptoSnowPrince <topdirector2017@gmail.com>
2023-07-24 22:40:45 +0900

    udpate

--- contracts/ElonmarsNFT.sol
+++ contracts/ElonmarsNFT.sol
@@ -10,12 +10,11 @@ import "./@openzeppelin/contracts/utils/Strings.sol";
 contract ElonmarsNFT is ERC1155, Ownable {
     using Strings for uint256;
 
-    uint256 public limit = 100;
     mapping(uint256 => uint256) public price; // tokenID => price
 
     IERC20 public payToken = IERC20(0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56);
-    address public treasury = 0x2faf8ab2b9ac8Bd4176A0B9D31502bA3a59B4b41; // CONFIG
-    address public admin = 0x2faf8ab2b9ac8Bd4176A0B9D31502bA3a59B4b41; // CONFIG
+    address public mintAdmin = 0x8e946b7453320383df75f080F7DA843c043DfB47; // CONFIG
+    address public withdrawAdmin = 0x2faf8ab2b9ac8Bd4176A0B9D31502bA3a59B4b41; // CONFIG
 
     mapping(uint256 => uint256) public totalSupply;
 
@@ -29,12 +28,7 @@ contract ElonmarsNFT is ERC1155, Ownable {
         require(_tokenID < 3, "Invalid Token ID");
         require(_quantity > 0, "Zero Amount");
 
-        if (msg.sender != admin) {
-            uint256 userBal = balanceOf(msg.sender, 0) +
-                balanceOf(msg.sender, 1) +
-                balanceOf(msg.sender, 2);
-            require(userBal + _quantity <= limit, "Balance LIMIT");
-
+        if (msg.sender != mintAdmin) {
             uint256 amount = price[_tokenID] * _quantity;
             payToken.transferFrom(msg.sender, address(this), amount);
         }
@@ -64,24 +58,20 @@ contract ElonmarsNFT is ERC1155, Ownable {
         price[2] = _rare_price;
     }
 
-    function updateLimit(uint256 _limit) external onlyOwner {
-        limit = _limit;
-    }
-
     function updatePayToken(IERC20 _payToken) external onlyOwner {
         payToken = _payToken;
     }
 
-    function updateTreasury(address _treasury) external onlyOwner {
-        treasury = _treasury;
+    function updateMintAdmin(address _mintAdmin) external onlyOwner {
+        mintAdmin = _mintAdmin;
     }
 
-    function updateAdmin(address _admin) external onlyOwner {
-        admin = _admin;
+    function updateWithdrawAdmin(address _withdrawAdmin) external onlyOwner {
+        withdrawAdmin = _withdrawAdmin;
     }
 
     function withdraw(address to, uint256 amount) external {
-        require(msg.sender == admin, "Invalid Admin");
+        require(msg.sender == withdrawAdmin, "Invalid Withdraw Admin");
 
         payToken.transfer(to, amount);
     }
